87a0058804ec0234f4bb43accfd4c013a868772b
Clayton Orman <corman.dev@gmail.com>
2024-05-20 12:00:42 -0400

    Update coordinates

--- src/components/store-finder/google-map.tsx
+++ src/components/store-finder/google-map.tsx
@@ -4,7 +4,7 @@ import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 import React from 'react';
 
 const GoogleMap = () => {
-  const position = { lat: 34.1763238, lng: -83.7870646 };
+  const position = { lat: 34.3257, lng: -83.3557 };
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY || ''}>
