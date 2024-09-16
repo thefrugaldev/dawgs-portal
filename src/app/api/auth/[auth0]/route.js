799a97e270fb5f16489022c794ee7e03042e471d
pursuitofdawgs <kyle.freese@uga.edu>
2024-09-16 16:33:42 -0500

    Added functionality for initial Auth0 authentication

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,11 +1,3 @@
-import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-const logoutUrl = [
-  `${process.env.AUTH0_ISSUER_BASE_URL}/v2/logout?`,
-  `client_id=${process.env.AUTH0_CLIENT_ID}`,
-  `&returnTo=${process.env.AUTH0_BASE_URL}`,
-];
-
-export const GET = handleAuth({
-    logout: handleLogout({ returnTo: logoutUrl.join('') }),
-  });
+export const GET = handleAuth();
