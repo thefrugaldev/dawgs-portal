18f98bc346dc462f5fa911ed1c68c3da278e2e36
Aaron Bemis <abemis@uga.edu>
2024-09-26 12:29:44 -0400

    formatting the table and adding all the fields

--- src/models/gas-station.ts
+++ src/models/gas-station.ts
@@ -4,7 +4,23 @@ export interface IGasStation {
   id: number;
   stationName: string;
   stationAddress: string;
-  gasPumps: number;
+  lat: Number;
+  long: Number;
+  gasPumps: Number;
+  dieselPumps: Number;
+  ecLvl2: Number;
+  ecFastDC: Number;
+  dcfc: Number;
+  ecCount: Number;
+  chargeCapacity: Number;
+  shopCount: Number;
+  shopNames: String;
+  truckStop: String;
+  bathroomStallCount: Number;
+  seatingAvailable: String;
+  greenspaceAvailable: String;
+  dailyCustomers: Number;
+  imageLink: String
 }
 
 const gasStationSchema = new Schema<IGasStation>({
@@ -20,10 +36,74 @@ const gasStationSchema = new Schema<IGasStation>({
     type: String,
     required: false,
   },
+  lat: {
+    type: Number,
+    required: false,
+  },
+  long: {
+    type: Number,
+    required: false,
+  },
   gasPumps: {
     type: Number,
     required: false,
   },
+  dieselPumps: {
+    type: Number,
+    required: false,
+  },
+  ecLvl2: {
+    type: Number,
+    required: false,
+  },
+  ecFastDC: {
+    type: Number,
+    required: false,
+  },
+  dcfc: {
+    type: Number,
+    required: false,
+  },
+  ecCount: {
+    type: Number,
+    required: false,
+  },
+  chargeCapacity: {
+    type: Number,
+    required: false,
+  },
+  shopCount: {
+    type: Number,
+    required: false,
+  },
+  shopNames: {
+    type: String,
+    required: false,
+  },
+  truckStop: {
+    type: String,
+    required: false,
+  },
+  bathroomStallCount: {
+    type: Number,
+    required: false,
+  },
+  seatingAvailable: {
+    type: String,
+    required: false,
+  },
+  greenspaceAvailable: {
+    type: String,
+    required: false,
+  },
+  dailyCustomers: {
+    type: Number,
+    required: false,
+  },
+  imageLink: {
+    type: String,
+    required: false,
+  }
 });
 
 export default models.gasstations || model('gasstations', gasStationSchema);
