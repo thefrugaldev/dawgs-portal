75ead47ecf98f5b97efa0048279c812486047dbf
Clayton Orman <corman.dev@gmail.com>
2024-10-14 20:10:10 -0400

    Search along routes and include polyline

--- src/components/store-finder/google-map.tsx
+++ src/components/store-finder/google-map.tsx
@@ -1,40 +1,28 @@
 'use client';
 
+import { searchAlongRoute } from '@/lib/google';
 import { Map, useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
+import {} from '@vis.gl/react-google-maps';
 import React, { useCallback, useEffect, useState } from 'react';
+import { Polyline } from '../google-maps/polyline';
 
 const GoogleMap = () => {
   const position = { lat: 34.3257, lng: -83.3557 };
   const map = useMap();
   const routesLib = useMapsLibrary('routes');
   const [route, setRoute] = useState<any>([]);
+  const [polyline, setPolyline] = useState<string>('');
 
-  const getGoogle = useCallback(async () => {
-    const { Place } = (await window.google.maps.importLibrary(
-      'places',
-    )) as google.maps.PlacesLibrary;
-    // const { AdvancedMarkerElement } = await window.google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-    const request = {
-      textQuery: 'Tacos in Mountain View',
-      searchAlongRouteParameters: {
-        polyline: {
-          encodedPolyline:
-            'wblcFptchVIFOd@G@EVw@Ms@dHKR}ApNA`AF~@Hf@TjAb@bBb@~@n@p@^Rd@~@Vz@HVz@nDLt@?d@Kr@c@~@mD`G?`@aEfGkCnDuChDm`@bb@[`@{GhHeEdEciBnnBkC`DkC~DaClEuKjT_Z|l@Qb@iR~_@}EzJ_AdB_Und@kAfCaOjZkg@vcAqBzD_]rr@iBlEaBxEgArD}AlG}AhHsA`IeAnH{@dIq@dJgL~iBq@rHu@vGgAtHwArHaBhHkBzG_DpJ}Nbc@iBhGkA|EgC|LcIjb@oAhG_AvDgAdDkApC_BzCiBpCsFvGii@vn@scAxlAmLjNgSzUeRjT{TzWqExEmG|FuNlMmMhLaRvPqOlNmbAl}@mFlF{PlOmJfIoElE}LtMiSbU_H`I}}@jcAwl@vp@oAbBqA~BeAhCm@tBg@fCWrBQ~BI|DaB~rBO~D[bEa@`Dm@pDaAdE{@vC_BbEkB~Def@|z@sEzHKJeS~]}K`S{\\~l@cXpe@sBpDm@bAuCxDkBrBiC~BwCtByBnAcBx@}Bt@{Bn@gh@|LaOpDeFhAoDj@aE^kVrA_E^iEr@yD~@uBr@gMjF_EnAcCh@eFr@_DRsAD}@Jsu@xCWDqIV}BCeCOyDm@cBa@_DmA}JeE_CwAsBcBiBoBuAqBmOoX{CuEkB_CoDqDkVoUoD{CeE_DkEkC_FeCqB}@sDuAoDgAeCe@cCW}CK}BDaDTeOlBcuBrYaNlBq@Dyd@rGyFt@yBb@eBf@oCnAoBlAkIpGkAp@wBbAaCt@oFdAwKjBoGxA{FbByIjC_HfB_@KmNdDuC|@uFzBcH|C{@\\[?sBv@}@VaBVoA@y@EmAQcA[w@]aBkAeAkA}BuDUKs@uAqBsCwBcCgAiAiN_MyKsJsG{GkBaBiBuA{BwAwDkBcOaHiC_AiCg@}BQcCAcBHqBVkB`@qEjAu@LgCVgAHwG@sG?mABsH^eNr@mBXy@NqBt@uAt@aBlAkAlA}BtCyApBiAdB_BxB{A`B}@j@oAf@s@PeCVcIf@gAAkAQy@YiAo@_A{@_DgEgJqM_DeEaM}PoBiCzAsBw@kAdAGVk@f@q@z@C',
-        },
-      },
-    };
-
-    //@ts-ignore
-    const { places } = await Place.searchByText(request);
-
-    if (places.length) {
-      console.log('PLACES: ', places);
-    }
+  const search = useCallback((polyline: string) => {
+    searchAlongRoute('Restaurants', polyline);
   }, []);
 
   useEffect(() => {
-    getGoogle();
-  }, [getGoogle]);
+    if (route.length > 0 && route[0].overview_polyline) {
+      setPolyline(route[0].overview_polyline);
+      search(route[0].overview_polyline);
+    }
+  }, [route, search]);
 
   useEffect(() => {
     if (!routesLib || !map) return;
@@ -54,7 +42,11 @@ const GoogleMap = () => {
 
   return (
     <Map defaultCenter={position} defaultZoom={10}>
-      {/* <Marker position={position} /> */}
+      <Polyline
+        encodedPath={polyline}
+        strokeWeight={10}
+        strokeColor={'#0f53ff'}
+      />
     </Map>
   );
 };
