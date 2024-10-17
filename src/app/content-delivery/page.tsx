abc270c688d0c8d6f610f5d7f503e3a8762766c8
Addison Hicks <ahicks.uga@gmail.com>
2024-10-17 17:04:51 -0400

    start of cdn integration 2

---
+++ src/app/content-delivery/page.tsx
@@ -0,0 +1,34 @@
+// app/content-delivery/page.tsx
+import Image from 'next/image'
+
+const getImage = async () => {
+  try {
+    const response = await fetch('api/aws/images');
+    console.log('Fetch response:', response);
+    
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image, status: ${response.status}`);
+    }
+
+    const data = await response.text();
+    console.log('Fetched image data:', data);
+    
+    return data;
+  } catch (error) {
+    console.error('Error fetching image:', error);
+  }
+}
+
+export default async function ScreenshotImage() {
+  const imageUrl = await getImage();
+
+  return (
+    <div>
+      {imageUrl ? (
+        <Image src={imageUrl} alt="s3url" width={1200} height={1200} />
+      ) : (
+        <p>Loading image...</p>
+      )}
+    </div>
+  );
+}
