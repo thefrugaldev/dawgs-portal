5de9447bd3b4859f914a65330bc14d4270602964
Clayton Orman <corman.dev@gmail.com>
2024-11-25 18:29:19 -0500

    Fix error

--- src/app/profile.js
+++ src/app/profile.js
@@ -1,30 +1,28 @@
-// profile.js
-/*
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { useEffect, useState } from 'react';
+// // profile.js
+// import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+// import { useEffect, useState } from 'react';
 
-const Profile = () => {
-  const [user, setUser] = useState(null);
+// const Profile = () => {
+//   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const response = await fetch('/api/auth/me');
-      const data = await response.json();
-      setUser(data);
-    };
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       const response = await fetch('/api/auth/me');
+//       const data = await response.json();
+//       setUser(data);
+//     };
 
-    fetchUser();
-  }, []);
+//     fetchUser();
+//   }, []);
 
-  if (!user) return <div>Loading...</div>;
+//   if (!user) return <div>Loading...</div>;
 
-  return (
-    <div>
-      <h1>Profile</h1>
-      {/* <pre>{JSON.sctringify(user, null, 2)}</pre> */}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Profile</h1>
+//       <pre>{JSON.sctringify(user, null, 2)}</pre>
+//     </div>
+//   );
+// };
 
-export default withPageAuthRequired(Profile);
-*/
+// export default withPageAuthRequired(Profile);
